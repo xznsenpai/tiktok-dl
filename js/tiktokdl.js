0167f43d65444f7525abe439dd6879c51f4e669c
Zeiyy <81281058+xznsenpai@users.noreply.github.com>
2024-01-17 15:45:52 +0800

    update js/tiktokdl.js

--- js/tiktokdl.js
+++ js/tiktokdl.js
@@ -33,15 +33,39 @@ document.addEventListener('DOMContentLoaded', function() {
 						tiktokContent.innerHTML += `<img src="${data.data.images[x]}" width="100%" height="25%"></img><br>`;
 					}
 				} else {
-					tiktokContent.innerHTML = `
-        <iframe src="${data.data.play}" width="100%" height="200px" frameborder="50"></iframe>
-        <h5 class="card-title">${formatK(data.data.digg_count)} Likes, ${formatK(data.data.comment_count)} Comments. TikTok video from ${data.data.author.nickname} (@${data.data.author.unique_id}): ${data.data.title}. ${data.data.music_info.title}</h5>
-        <p class="card-text download-buttons">
-          <button class="btn btn-success" onclick="window.open('${data.data.play}', '_blank')">Download SD</button>
-          <button class="btn btn-warning" onclick="window.open('${data.data.hdplay}', '_blank')">Download HD</button>
-          <button class="btn btn-info" onclick="window.open('${data.data.music}', '_blank')">Download Music</button>
-        </p>
-      `;
+					let result = `<div class="card-body">
+      	<div class="d-flex align-items-center mb-3">
+      		<a href="#">
+      			<img src="${data.data.author.avatar}" alt="" width="50" class="rounded-circle">
+      		</a>
+      		<div class="flex-fill ps-2">
+      			<div class="fw-bold">
+      				<a href="#" class="text-decoration-none">${data.data.author.nickname}</a>
+      				<a href="#" class="text-decoration-none">${data.data.author.unique_id}</a>
+      			</div>
+      			<div class="small text-inverse text-opacity-50">${data.data.id}</div>
+      		</div>
+      	</div>
+      	<p>${data.data.title}</p>
+      	<div class="ratio ratio-16x9">
+      		<iframe src="${data.data.play}"></iframe>
+      	</div>
+      	<hr class="mb-1">
+      	<div class="row text-center fw-bold">
+      		<div class="col"> Like <br> ${formatK(data.data.digg_count)} </div>
+      		<div class="col"> Comment <br> ${formatK(data.data.comment_count)} </div>
+      		<div class="col"> Share <br> ${formatK(data.data.share_count)} </div>
+      	</div>
+      	<hr class="mb-3 mt-1">
+      	<div class="row text-center fw-bold">
+      		<div class="col">
+      			<a href="${data.data.hdplay}" type="button" class="btn btn-success btn-sm" target="_self"> HD PLAY </a>
+      			<a href="${data.data.play}" type="button" class="btn btn-danger btn-sm" target="_self"> SD PLAY </a>
+      			<a href="${data.data.music}" type="button" class="btn btn-warning btn-sm" target="_self"> MUSIC </a>
+      		</div>
+      	</div>
+      </div>`
+					tiktokContent.innerHTML = result
 				}
 			})
 			.catch(error => {
